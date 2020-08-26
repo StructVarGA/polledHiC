55154430e2c8b5826c8cc7b0cc3facf4c0820ec4
Jeremy Martin <jeremy.delpech.martin@gmail.com>
2020-08-26 19:29:02 +0200

    Update

--- workflows/hicexplorer_jm/run.sh
+++ workflows/hicexplorer_jm/run.sh
@@ -5,8 +5,8 @@
 #SBATCH -p workq
 #SBATCH --mail-type=END,FAIL
 #SBATCH --export=ALL
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
 
 module purge
 module load system/Miniconda3-4.7.10
@@ -30,6 +30,39 @@ sleep 0.4s
 echo "Run of Snakemake"
 sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo '-'
 
-snakemake --jobs 2 -p \
+snakemake --jobs 1 -p \
   --snakefile /work2/genphyse/dynagen/jmartin/polledHiC/workflows/hicexplorer_jm/Snakefile \
   --directory /work2/genphyse/dynagen/jmartin/polledHiC/workflows/hicexplorer_jm/
+
+echo "DONE"
+echo ""
+
+echo "Sum of all indivs. to get 'Consensus matrices'"
+sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo -n '-' ; sleep 0.4s ; echo '-'
+
+mkdir -p hdf5/consensus
+
+getRes=$(ls -d hdf5/*/merged/* | cut -f 4 -d / | sort | uniq)
+
+for res in $getRes
+do
+  echo "Matrices to sum :"
+  matrices=$(ls hdf5/*/merged/$res/corrected.h5)
+  echo $matrices
+  echo ""
+  
+  hicSumMatrices -m $matrices -o hdf5/consensus/$res.h5
+  
+  echo "Consensus $res DONE !"
+  echo ""
+done
+
+# # to plot maps from consensus matrices, please fill {} and uncomment followings lines :
+# 
+# mkdir -p plots/consensus
+# 
+# for mat in hdf5/consensus/*.h5
+# do
+#   out=${mat##*/}
+#   hicPlotMatrix -m $mat -o plots/consensus/${out%.h5}.png --region {chromosome}:{start}-{end} --log
+# done
